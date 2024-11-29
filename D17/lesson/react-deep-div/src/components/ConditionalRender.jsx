94cb4cdb4d2e3f1eafa293460a9227d13032a62f
Mike Foronda <mike.p.foronda@gmail.com>
2024-11-29 19:15:30 +0800

    D17 add dependencies

--- D17/lesson/react-deep-div/src/components/ConditionalRender.jsx
+++ D17/lesson/react-deep-div/src/components/ConditionalRender.jsx
@@ -1,17 +1,15 @@
-import React, {setState, useState} from 'react';
+import React, { useState } from 'react';
 
 const ConditionalRender = () => {
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
-
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   return (
     <div>
-        { isLoggedIn 
-            ? <p>Welcome Back!</p> 
-            : <p>Please Log in</p>
-        }
-        <button onClick={()=> setIsLoggedIn(!isLoggedIn)}>Click Me</button>
+      {isLoggedIn ? <p>Welcome Back!</p> : <p>Please Log In</p>}
+      <button onClick={() => setIsLoggedIn(!isLoggedIn)}>
+        {isLoggedIn ? 'Log Out' : 'Log In'}
+      </button>
     </div>
-  )
-}
+  );
+};
 
 export default ConditionalRender;
